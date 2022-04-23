c659bd97754392910624fcd4239a7b9a24be5a26
jaylnne <jaylnne@zuminternet.com>
2022-04-23 01:51:23 +0000

    feat: LightningModule 클래스 생성

---
+++ model.py
@@ -0,0 +1,68 @@
+from transformers import BertModel
+import pytorch_lightning as pl
+import torch.nn.functional as F
+
+
+class NSMCClassification(pl.LightningModule):
+    
+    def __init__(self):
+        super(NSMC_Finetuner, self).__init__()
+        
+        # load pretrained koBERT
+        self.bert = BertModel.from_pretrained('skt/kobert-base-v1', output_attentions=True)
+        
+        # simple linear layer (긍/부정, 2 classes)
+        self.W = nn.Linear(bert.config.hidden_size, 2)
+        self.num_classes = 2
+        
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        
+        h, _, attn = self.bert(input_ids=input_id,
+                               attention_mask=attention_mask,
+                               token_type_ids=token_type_ids,
+                              )
+        h_cls = h[:, 0]
+        logits = self.W(h_cls)
+        
+        return logits, attn
+    
+    def training_step(self, batch, batch_nb):
+        # batch
+        input_ids, attention_mask, token_type_ids, label = batch
+        
+        # forward
+        y_hat, attn = self.forward(input_ids, attention_mask, token_type_ids)
+        
+        # BCE loss
+        loss = F.binary_cross_entropy_with_logits(y_hat, label)
+        
+        # logs
+        tensorboard_logs = {'train_loss': loss}
+        
+        return {'loss': loss, 'log': tensorboard_logs}
+    
+    def validation_step(self, batch, batch_nb):
+        # batch
+        input_ids, attention_mask, token_type_ids, label = batch
+        
+        # forward
+        y_hat, attn = self.forward(input_ids, attention_mask, token_type_ids)
+        
+        # loss
+        loss = F.binary_cross_entropy_with_logits(y_hat, label)
+        
+        # accuracy
+        a, y_hat = torch.max(y_hat, dim=1)
+        val_acc = accuracy_score(y_hat.cpu(), label.cpu())
+        val_acc = torch.tensor(val_acc)
+        
+        return {'val_loss': loss, 'val_acc': val_acc}
+    
+    def validation_end(self, outputs):
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        avg_val_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
+        
+        tensorboard_logs = {'val_loss': avg_loss, 'avg_val_acc':avg_val_acc}
+        return {'avg_val_loss': avg_logg, 'progress_bar': tensorboard_logs}
+    
+    
