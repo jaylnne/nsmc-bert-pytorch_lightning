90d240b7151f4a10b2b2998c6d0b08aba4fe8c9a
jaylnne <jaylnne@zuminternet.com>
2022-04-24 13:13:25 +0000

    feat: optimizer 세팅 & NSMCDataModule 추가

--- model.py
+++ model.py
@@ -1,18 +1,22 @@
-from transformers import BertModel
-import pytorch_lightning as pl
+import torch
+## TODO randomness 제어하기 -> torch, CuDNN etc
+import torch.nn as nn
 import torch.nn.functional as F
+import pytorch_lightning as pl
+from transformers import BertModel#, BertTokenizer
+from torch.utils.data import random_split, DataLoader
 
 
 class NSMCClassification(pl.LightningModule):
     
     def __init__(self):
-        super(NSMC_Finetuner, self).__init__()
+        super(NSMCClassification, self).__init__()
         
         # load pretrained koBERT
         self.bert = BertModel.from_pretrained('skt/kobert-base-v1', output_attentions=True)
         
         # simple linear layer (긍/부정, 2 classes)
-        self.W = nn.Linear(bert.config.hidden_size, 2)
+        self.W = nn.Linear(self.bert.config.hidden_size, 2)
         self.num_classes = 2
         
     def forward(self, input_ids, attention_mask, token_type_ids):
@@ -62,7 +66,7 @@ class NSMCClassification(pl.LightningModule):
         avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
         avg_val_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
         
-        tensorboard_logs = {'val_loss': avg_loss, 'avg_val_acc':avg_val_acc}
+        tensorboard_logs = {'val_loss': avg_loss,'avg_val_acc':avg_val_acc}
         return {'avg_val_loss': avg_loss, 'progress_bar': tensorboard_logs}
     
     def test_step(self, batch, batch_nb):
@@ -81,3 +85,46 @@ class NSMCClassification(pl.LightningModule):
         
         tensorboard_logs = {'avg_test_acc': avg_test_acc}
         return {'avg_test_acc': tensorboard_logs}
+    
+    def configure_optimizer(self):
+        parameters = []
+        for p in self.parameters():
+            if p.requires_grad:
+                parameters.append(p)
+            else:
+                print(p)
+                
+        optimizer = torch.optim.Adam(parameters, lr=2e-05, eps=1e-08)
+        
+        return optimizer
+    
+
+# TODO NSMCDataset 클래스 추가
+    
+    
+class NSMCDataModule(pl.LightningDataModule):
+    
+    def __init__(self, data_dir, stem_analyzer, valid_size, max_seq_len, batch_size):
+        self.full_data_path = f'{data_dir}/train_{self.stem_analyzer}.csv'
+        self.test_data_path = f'{data_dir}/test_{self.stem_analyzer}.csv'
+        self.valid_size = valid_size
+        self.max_seq_len = max_seq_len
+        self.batch_size = batch_size
+        
+    def setup(self, stage):
+        full = NSMCDataset(self.full_data_path, self.max_seq_len)
+        train_size = int(len(full) * (1 - self.valid_size))
+        valid_size = len(full) - train_size
+        self.train, self.valid = random_split(nsmc_full, [train_size, valid_size])
+        self.test = NSMCDataset(self.test_data_path, self.max_seq_len)
+        
+    def train_dataloader(self):
+        return DataLoader(self.train, batch_size=64, num_workers=5, shuffle=True, pin_memory=True)
+    
+    def val_dataloader(self):
+        return DataLoader(self.valid, batch_size=64, num_workers=5, shuffle=False, pin_memory=True)
+    
+    def test_dataloader(self):
+        return DataLoader(self.test, batch_size=64, num_workers=5, shuffle=False, pin_memory=True)
+    
+    ## TODO predict_dataloader 추가
