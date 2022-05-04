a007f00180f79997008648b8de7ae544a6e59e0a
jaylnne <jaylnne@zuminternet.com>
2022-05-04 13:26:16 +0000

    refact: argparse 추가

--- test.py
+++ test.py
@@ -3,22 +3,45 @@ from pytorch_lightning import Trainer, seed_everything
 
 from model import *
 
-
-seed_everything(42, workers=True)
-
-AVAIL_GPUS = -1
-STEM_ANALYZER = 'mecab'
-CKPT_PATH = 'checkpoints/epoch=01-val_acc=0.866.ckpt'
-VALID_SIZE = 0.1
-MAX_SEQ_LEN = 64
-BATCH_SIZE = 32
+parser = argparse.ArgumentParser()
+parser.add_argument('--seed',
+                   type=int,
+                   default=42)
+parser.add_argument('--data_path',
+                    type=str,
+                    default='data',
+                    help='where to prepare data')
+parser.add_argument('--num_gpus',
+                   type=int,
+                   default=-1,
+                   help='number of available gpus')
+parser.add_argument('--mode',
+                   type=str,
+                   default='clean',
+                   choices=['clean', 'only_korean'])
+parser.add_argument('--ckpt_path',
+                   type=str,
+                   help='checkpoint file path')
+parser.add_argument('--valid_size',
+                   type=float,
+                   default=0.1,
+                   help='size of validation file')
+parser.add_argument('--max_seq_len',
+                   type=int,
+                   help='maximum length of input sequence data')
+parser.add_argument('--batch_size',
+                   type=int,
+                   help='batch size')
+args = parser.parse_args()
+    
+seed_everything(args.seed, workers=True)
 
 dm = NSMCDataModule(
-    data_dir='./data', 
-    stem_analyzer=STEM_ANALYZER, 
-    valid_size=VALID_SIZE, 
-    max_seq_len=MAX_SEQ_LEN, 
-    batch_size=BATCH_SIZE,
+    data_dir=args.data_path, 
+    mode=args.mode, 
+    valid_size=args.valid_size, 
+    max_seq_len=args.max_seq_len, 
+    batch_size=args.batch_size,
 )
 dm.setup('test')
 
@@ -27,7 +50,7 @@ model = NSMCClassification()
 trainer = Trainer(
     accelerator='gpu',
     strategy="ddp",
-    devices=AVAIL_GPUS,
+    devices=args.num_gpus,
     auto_select_gpus=True,
 )
-trainer.test(model, dm, ckpt_path=CKPT_PATH)
+trainer.test(model, dm, ckpt_path=args.ckpt_path)
